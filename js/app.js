9b468872ef6c765bd1283266e80a327c2c320378
afnandamra <damra.afnan@gmail.com>
2021-01-11 17:00:45 +0200

    checkpoint

--- js/app.js
+++ js/app.js
@@ -10,171 +10,120 @@ for (var j = 1; j <= 7; j++) {
 
 console.log(openHour);
 
-function randomCus(min, max) {
+
+
+function randomNum(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-var seattle = {
-    name: 'Seattle',
-    minCusPerHour: 23,
-    maxCusPerHour: 65,
-    avgCookiePerCus: 6.3,
-    avgCusPerHour: [],
-    getCusPerHour: function () {
-        for (var m = 0; m < openHour.length; m++) {
-            this.avgCusPerHour.push(randomCus(this.minCusPerHour, this.maxCusPerHour));
-            // console.log(this.avgCusPerHour[m]);
-        }
-        return this.avgCusPerHour;
-    },
-    avgCookiesPerHour: [],
-    totalCookies: 0,
-    getAvgCookiesPerHour: function () {
-        for (var k = 0; k < openHour.length; k++) {
-            this.avgCookiesPerHour.push(Math.ceil(this.avgCusPerHour[k] * this.avgCookiePerCus));
-            this.totalCookies = this.totalCookies + this.avgCookiesPerHour[k];
-        }
-        // for (var l = 0; l < openHour.length; l++) {
-        //     console.log(openHour[l], ': ', this.avgCookiesPerHour[l]);
-        // }
-        // console.log('total: ', this.totalCookies);
-        return this.avgCookiesPerHour, this.totalCookies;
+function Shop(shopName, minCusPerHour, maxCusPerHour, avgCookiePerCus) {
+    this.name = shopName;
+    this.minCusPerHour = minCusPerHour;
+    this.maxCusPerHour = maxCusPerHour;
+    this.avgCookiePerCus = avgCookiePerCus;
+    this.avgCusPerHour = [];
+    this.totalCookies = 0;
+}
+
+Shop.prototype.getCusPerHour = function () {
+    for (var m = 0; m < openHour.length; m++) {
+        this.avgCusPerHour.push(randomNum(this.minCusPerHour, this.maxCusPerHour));
+        // console.log(this.avgCusPerHour[m]);
     }
 }
 
-var tokyo = {
-    name: 'Tokyo',
-    minCusPerHour: 3,
-    maxCusPerHour: 24,
-    avgCookiePerCus: 1.2,
-    avgCusPerHour: [],
-    getCusPerHour: function () {
-        for (var m = 0; m < openHour.length; m++) {
-            this.avgCusPerHour.push(randomCus(this.minCusPerHour, this.maxCusPerHour));
-            // console.log(this.avgCusPerHour[m]);
-        }
-        return this.avgCusPerHour;
-    },
-    avgCookiesPerHour: [],
-    totalCookies: 0,
-    getAvgCookiesPerHour: function () {
-        for (var k = 0; k < openHour.length; k++) {
-            this.avgCookiesPerHour.push(Math.ceil(this.avgCusPerHour[k] * this.avgCookiePerCus));
-            this.totalCookies = this.totalCookies + this.avgCookiesPerHour[k];
-        }
-        // for (var l = 0; l < openHour.length; l++) {
-        //     console.log(openHour[l], ': ', this.avgCookiesPerHour[l]);
-        // }
-        // console.log('total: ', this.totalCookies);
-        return this.avgCookiesPerHour, this.totalCookies;
+Shop.prototype.getAvgCookiesPerHour = function () {
+    var avgCookiesPerHour = [];
+    for (var k = 0; k < openHour.length; k++) {
+        avgCookiesPerHour.push(Math.ceil(this.avgCusPerHour[k] * this.avgCookiePerCus));
+        this.totalCookies = this.totalCookies + avgCookiesPerHour[k];
     }
+    // for (var l = 0; l < openHour.length; l++) {
+    //     console.log(openHour[l], ': ', avgCookiesPerHour[l]);
+    // }
+    console.log('total: ', this.totalCookies);
 }
 
-var dubai = {
-    name: 'Dubai',
-    minCusPerHour: 11,
-    maxCusPerHour: 38,
-    avgCookiePerCus: 3.7,
-    avgCusPerHour: [],
-    getCusPerHour: function () {
-        for (var m = 0; m < openHour.length; m++) {
-            this.avgCusPerHour.push(randomCus(this.minCusPerHour, this.maxCusPerHour));
-            // console.log(this.avgCusPerHour[m]);
-        }
-        return this.avgCusPerHour;
-    },
-    avgCookiesPerHour: [],
-    totalCookies: 0,
-    getAvgCookiesPerHour: function () {
-        for (var k = 0; k < openHour.length; k++) {
-            this.avgCookiesPerHour.push(Math.ceil(this.avgCusPerHour[k] * this.avgCookiePerCus));
-            this.totalCookies = this.totalCookies + this.avgCookiesPerHour[k];
-        }
-        // for (var l = 0; l < openHour.length; l++) {
-        //     console.log(openHour[l], ': ', this.avgCookiesPerHour[l]);
-        // }
-        // console.log('total: ', this.totalCookies);
-        return this.avgCookiesPerHour, this.totalCookies;
-    }
-}
 
-var paris = {
-    name: 'Paris',
-    minCusPerHour: 20,
-    maxCusPerHour: 38,
-    avgCookiePerCus: 2.3,
-    avgCusPerHour: [],
-    getCusPerHour: function () {
-        for (var m = 0; m < openHour.length; m++) {
-            this.avgCusPerHour.push(randomCus(this.minCusPerHour, this.maxCusPerHour));
-            // console.log(this.avgCusPerHour[m]);
-        }
-        return this.avgCusPerHour;
-    },
-    avgCookiesPerHour: [],
-    totalCookies: 0,
-    getAvgCookiesPerHour: function () {
-        for (var k = 0; k < openHour.length; k++) {
-            this.avgCookiesPerHour.push(Math.ceil(this.avgCusPerHour[k] * this.avgCookiePerCus));
-            this.totalCookies = this.totalCookies + this.avgCookiesPerHour[k];
-        }
-        // for (var l = 0; l < openHour.length; l++) {
-        //     console.log(openHour[l], ': ', this.avgCookiesPerHour[l]);
-        // }
-        // console.log('total: ', this.totalCookies);
-        return this.avgCookiesPerHour, this.totalCookies;
-    }
-}
+Shop.prototype.render = function () {
+    var parentSec = document.getElementById('mainId');
+    // section
+    var locationSection = document.createElement('section');
+    parentSec.appendChild(locationSection);
+    //Name h3
+    var tableName = document.createElement('h3');
+    tableName.textContent = 'Salmon Cookie Sales';
+    locationSection.appendChild(tableName);
+    //table
 
-var lima = {
-    name: 'Lima',
-    minCusPerHour: 2,
-    maxCusPerHour: 16,
-    avgCookiePerCus: 4.6,
-    avgCusPerHour: [],
-    getCusPerHour: function () {
-        for (var m = 0; m < openHour.length; m++) {
-            this.avgCusPerHour.push(randomCus(this.minCusPerHour, this.maxCusPerHour));
-            // console.log(this.avgCusPerHour[m]);
-        }
-        return this.avgCusPerHour;
-    },
-    avgCookiesPerHour: [],
-    totalCookies: 0,
-    getAvgCookiesPerHour: function () {
-        for (var k = 0; k < openHour.length; k++) {
-            this.avgCookiesPerHour.push(Math.ceil(this.avgCusPerHour[k] * this.avgCookiePerCus));
-            this.totalCookies = this.totalCookies + this.avgCookiesPerHour[k];
-        }
-        // for (var l = 0; l < openHour.length; l++) {
-        //     console.log(openHour[l], ': ', this.avgCookiesPerHour[l]);
-        // }
-        // console.log('total: ', this.totalCookies);
-        return this.avgCookiesPerHour, this.totalCookies;
-    }
+    locationSection.appendChild(locationTable);
+    // table rows
+    this.header();
+    //5 shop rows tableRow[1-5]
+    var locationRows = [1, 2, 3, 4, 5];
+
+    //last row tableRow[6]
 }
+var locationTable = document.createElement('table');
+Shop.prototype.header = function () {
+    var headRow = document.createElement('thead');
+    locationTable.appendChild(headRow);
+    var emptyCell = document.createElement('th');
+    headRow.appendChild(emptyCell);
+    var headRowCells = [emptyCell];
+    for (var j = 1; j <= openHour.length; j++) {
+        headRowCells.push(document.createElement('th'));
+        headRowCells[j].textContent = openHour[j - 1];
+        headRow.appendChild(headRowCells[j]);
+    }
+    var totalDaily = document.createElement('th');
+    totalDaily.textContent = 'Daily Location Total';
+    headRow.appendChild(totalDaily);
+}
+// for (var o = 0; o < locations.length; o++) {
+//     locations[o].getCusPerHour();
+//     locations[o].getAvgCookiesPerHour();
+//     var parent = document.getElementById('mainId');
+//     var locationName = document.createElement('h3');
+//     locationName.textContent = locations[o].name;
+//     parent.appendChild(locationName);
+
+//     var locationCookies = document.createElement('ul');
+//     locationCookies.setAttribute('id', 'hours');
+//     parent.appendChild(locationCookies);
+
+//     var locationList = [];
+//     for (var n = 0; n < openHour.length + 1; n++) {
+//         locationList = document.createElement('li');
+//         locationList.setAttribute('class', 'hours');
+//         locationCookies.appendChild(locationList);
+//         locationList.textContent = (openHour[n] + ': ' + locations[o].avgCookiesPerHour[n] + ' cookies');
+//     }
+//     locationList.textContent = ('total: ' + locations[o].totalCookies + ' cookies');
+
+// }
+
+var seattle = new Shop('Seattle', 23, 65, 6.3);
+console.log(seattle);
+
+var tokyo = new Shop('Tokyo', 3, 24, 1.2);
+console.log(tokyo);
+
+var dubai = new Shop('Dubai', 11, 38, 3.7);
+console.log(dubai);
+
+var paris = new Shop('Paris', 20, 38, 2.3);
+console.log(paris);
+
+var lima = new Shop('Lima', 2, 16, 4.6);
+console.log(lima);
 
 var locations = [seattle, tokyo, dubai, paris, lima];
+console.log(locations);
 
-for (var o = 0; o < locations.length; o++) {
-    locations[o].getCusPerHour();
-    locations[o].getAvgCookiesPerHour();
-    var parent = document.getElementById('mainId');
-    var locationName = document.createElement('h3');
-    locationName.textContent = locations[o].name;
-    parent.appendChild(locationName);
-
-    var locationCookies = document.createElement('ul');
-    locationCookies.setAttribute('id', 'hours');
-    parent.appendChild(locationCookies);
-
-    var locationList = [];
-    for (var n = 0; n < openHour.length + 1; n++) {
-        locationList = document.createElement('li');
-        locationList.setAttribute('class', 'hours');
-        locationCookies.appendChild(locationList);
-        locationList.textContent = (openHour[n] + ': ' + locations[o].avgCookiesPerHour[n] + ' cookies');
-    }
-    locationList.textContent = ('total: ' + locations[o].totalCookies + ' cookies');
-
+for (var i = 0; i < locations.length; i++) {
+    locations[i].getCusPerHour();
+    locations[i].getAvgCookiesPerHour();
 }
+seattle.render();
+
---
+++ js/table.md
@@ -0,0 +1,128 @@
+<table>
+    <tr>
+        <th></th>
+        <th>6 am</th>
+        <th>7 am</th>
+        <th>8 am</th>
+        <th>9 am</th>
+        <th>10 am</th>
+        <th>11 am</th>
+        <th>12 pm</th>
+        <th>1 pm</th>
+        <th>2 pm</th>
+        <th>3 pm</th>
+        <th>4 pm</th>
+        <th>5 pm</th>
+        <th>6 pm</th>
+        <th>7 pm</th>
+        <th>daily location total</th>
+    </tr>
+    <tr>
+        <th>seattle</th>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+    </tr>
+    <tr>
+        <th>tokyo</th>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+    </tr>
+    <tr>
+        <th>dubai</th>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+    </tr>
+    <tr>
+        <th>paris</th>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+    </tr>
+    <tr>
+        <th>lima</th>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+    </tr>
+    <tr>
+        <th>total</th>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+        <td>cell</td>
+    </tr>
+</table>
